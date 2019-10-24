9ba804eb07073028690c776834ad76d5dcd78555
francois.moine <francois.moine@epitech.eu>
2019-10-24 16:49:30 +0200

    add login session to blih

--- build_sources/bin/hash_passwd.py
+++ build_sources/bin/hash_passwd.py
@@ -7,12 +7,22 @@ import getpass
 
 file_path = "~/.zsh/zshrc_login"
 login = input("Enter your login: ")
+passwd = hashlib.sha512(bytes(getpass.getpass(), 'utf8')).hexdigest()
+
+os.environ['BLIH_USER'] = login;
+os.environ['BLIH_TOKEN'] = passwd;
+cmd = 'blih repository list'
+is_connect = os.popen(cmd).read()
+
+if is_connect.find('Bad token') != -1:
+    print("Fail to connect bad token, Retry !")
+    exit(84)
+else:
+    print("Success full connected as " + login)
+
 cmd = 'echo "export BLIH_USER=\"' + login + '\"" > ' + file_path
 os.system(cmd)
-#subprocess.call(cmd, shell=True)
-passwd = hashlib.sha512(bytes(getpass.getpass(), 'utf8')).hexdigest()
 cmd = 'echo "export BLIH_TOKEN=\"' + passwd + '\"" >> ' + file_path
 os.system(cmd)
-cmd = 'source ~/.zsh/zshrc_login; zsh -i'
+cmd = 'echo "source ~/.zsh/zshrc_login" >> ~/.zshrc ; zsh -i'
 os.system(cmd)
-
